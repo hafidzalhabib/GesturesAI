e7648dffce139ff578722aa65e770f8b08bbd3c9
Mohamad Hafidz Al Habib <98099534+hafidzalhabib@users.noreply.github.com>
2025-09-12 09:37:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
 import cv2
- # Import Library
-import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import  numpy as np
-import google.generativeai as genai
-from PIL import Image
 
 st.set_page_config(layout="wide")
 col_1, col_2 = st.columns([2,3])
@@ -16,34 +12,26 @@ with col_1:
     input_text = st.text_input(label="Masukkan perintah")
     st.subheader("Jawaban")
     output_text_area = st.text("   ")
-
-
 with col_2:
-    # Buat detector sekali saja, jangan di dalam callback
-    detector = HandDetector(
-        staticMode=False, 
-        maxHands=1, 
-        modelComplexity=1, 
-        detectionCon=0.5, 
-        minTrackCon=0.5
-    )
 
     def callback(frame):
+        detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+
         img = frame.to_ndarray(format="bgr24")
 
         hands, img = detector.findHands(img, draw=True, flipType=True)
 
+        # Check if any hands are detected
         if hands:
-            hand1 = hands[0]
-            lmList1 = hand1["lmList"]  
-            bbox1 = hand1["bbox"]  
-            center1 = hand1['center']  
-            handType1 = hand1["type"]  
-
-            # Contoh update text di kolom kiri
-            output_text_area.text(f"Terdeteksi {handType1} hand di {center1}")
+            # Information for the first hand detected
+            hand1 = hands[0]  # Get the first hand detected
+            lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+            bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
+            center1 = hand1['center']  # Center coordinates of the first hand
+            handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
+
 
     webrtc_streamer(
         key="example",
@@ -62,6 +50,7 @@ with col_2:
         }
     )
  
+
 
 
 
